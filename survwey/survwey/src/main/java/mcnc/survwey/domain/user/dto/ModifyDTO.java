1ba7996bb360cefcf9b8c305ef1fbc64d78ea362
kangminlee1 <jack7853@naver.com>
2024-11-25 17:11:08 +0900

    feat: 프로필 조회 로직 구현

--- survwey/survwey/src/main/java/mcnc/survwey/domain/user/dto/ModifyDTO.java
+++ survwey/survwey/src/main/java/mcnc/survwey/domain/user/dto/ModifyDTO.java
@@ -1,6 +1,8 @@
 package mcnc.survwey.domain.user.dto;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import mcnc.survwey.domain.enums.Gender;
@@ -8,12 +10,16 @@ import mcnc.survwey.domain.enums.Gender;
 import java.time.LocalDate;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ModifyDTO {
+    private String name;
+
+    private String email;
 
     private LocalDate birth;
 
     private Gender gender;
 
-    private String name;
 }
