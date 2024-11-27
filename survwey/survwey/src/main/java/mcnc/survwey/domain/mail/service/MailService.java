ee6c49c324bbe83de128b8282fef1380119e8f1d
kangminlee1 <jack7853@naver.com>
2024-11-27 10:49:46 +0900

    refactor: MailService 리팩토링

--- survwey/survwey/src/main/java/mcnc/survwey/domain/mail/service/MailService.java
+++ survwey/survwey/src/main/java/mcnc/survwey/domain/mail/service/MailService.java
@@ -11,7 +11,6 @@ import mcnc.survwey.domain.user.User;
 import mcnc.survwey.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -22,8 +21,6 @@ import org.thymeleaf.context.Context;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Random;
-
 
 @Service
 @Slf4j
@@ -41,6 +38,30 @@ public class MailService {
     private String senderEmail;
     @Value("${BASE_URL}")
     private String baseUrl;
+    @Value("${SURVEY_VERIFY_URL}")
+    private String notificationUrl;
+    private static final String LOGO_IMAGE_PATH = "static/images/icon_logo.png";
+    private static final String TITLE_IMAGE_PATH = "static/images/title.png";
+
+    //메일 보내는 메소드
+    public void mailSender(Context context, String title, String email, String link) throws MessagingException {
+        String htmlContent = templateEngine.process("mail/send", context);//타임리프 템플릿 처리 후 HTML 콘텐츠 최종 생성
+        MimeMessage message = mailSender.createMimeMessage();// 이메일 메시지 생성 객체
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);// T: html 형식, F: 텍스트 형식
+
+        helper.setFrom(senderEmail);
+        helper.setSubject(title);
+        helper.setTo(email);
+        helper.setText(htmlContent, true);
+
+        // 이미지 첨부 (첨부파일로 cid를 사용)
+        ClassPathResource logoResource = new ClassPathResource(LOGO_IMAGE_PATH);
+        helper.addInline("logoImage", logoResource); // 이미지 ID 'logoImage'로 첨부
+        ClassPathResource titleResource = new ClassPathResource(TITLE_IMAGE_PATH);
+        helper.addInline("titleImage", titleResource); // 이미지 ID 'titleImage'로 첨부
+        log.info("link = {}", link); //테스트용
+        mailSender.send(message);
+    }
 
     /**
      * 설문 초대
@@ -58,34 +79,16 @@ public class MailService {
 
         try{
             LocalDateTime surveyExpireDay = survey.getExpireDate();
-
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd a h:mm");
-
+            //그거를 파라미터를 Map으로 받게 하면 되긴함 파라미터를 Map<String, Object> 이런식으로 받아서
+            //임시 리팩토링 추후 생각
             Context context = new Context();//타임리프 템플릿에 전달할 데이터 저장하는 컨테이너
             context.setVariable("inviterName", user.getName());
-            context.setVariable("expireDate", surveyExpireDay.format(formatter));
+            context.setVariable("surveyTitle", survey.getTitle());
             context.setVariable("surveyLink", link);
+            context.setVariable("expireDate", surveyExpireDay.format(formatter));
 
-            String htmlContent = templateEngine.process("mail/send", context);//타임리프 템플릿 처리 후 HTML 콘텐츠 최종 생성
-
-            MimeMessage message = mailSender.createMimeMessage();// 이메일 메시지 생성 객체
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);// T: html 형식, F: 텍스트 형식
-
-            helper.setFrom(senderEmail);
-            helper.setSubject(survey.getTitle());
-            helper.setTo(user.getEmail());
-            helper.setText(htmlContent, true);
-
-            // 이미지 첨부 (첨부파일로 cid를 사용)
-            ClassPathResource logoResource = new ClassPathResource("static/images/icon_logo.png");
-            helper.addInline("logoImage", logoResource); // 이미지 ID 'logoImage'로 첨부
-
-            ClassPathResource titleResource = new ClassPathResource("static/images/title.png");
-            helper.addInline("titleImage", titleResource); // 이미지 ID 'titleImage'로 첨부
-
-            log.info("link = {}", link);
-            mailSender.send(message);
-
+            mailSender(context, survey.getTitle(), user.getEmail(), link);
         } catch (MailException | MessagingException e){
             throw new RuntimeException("메일 발송 실패 ", e);
         } catch (Exception e) {
@@ -103,7 +106,6 @@ public class MailService {
         Survey survey = surveyService.findBySurveyId(surveyId);//설문 존재 하는지 확인
         surveyService.validateUserMadeSurvey(userId, survey);//사용자 본인이 만들었는지 확인
         surveyService.checkSurveyExpiration(survey.getExpireDate());//만료일 확인
-
         String encryptedSurveyId = encryptionUtil.encrypt(surveyId.toString());
         return baseUrl + encryptedSurveyId;
     }
@@ -116,7 +118,6 @@ public class MailService {
     public String decryptedLink(String surveyId){
         Survey survey = surveyService.findBySurveyId(Long.parseLong(surveyId));//설문이 존재하는지 확인
         surveyService.checkSurveyExpiration(survey.getExpireDate());//만료일 확인
-
         return baseUrl + surveyId;
     }
 
@@ -127,38 +128,15 @@ public class MailService {
      * @param link
      */
     public void sendVerifySurveyLink(String userId, Long surveyId, String link){
-
         User user = userService.findByUserId(userId);
         Survey survey = surveyService.findBySurveyId(surveyId);
-
         surveyService.validateUserMadeSurvey(userId, survey);
         //본인이 생성한 설문 확인
-
         try{
             Context context = new Context();//타임리프 템플릿에 전달할 데이터 저장하는 컨테이너
             context.setVariable("inviterName", user.getName());
             context.setVariable("surveyLink", link);
-
-            String htmlContent = templateEngine.process("mail/notification", context);//타임리프 템플릿 처리 후 HTML 콘텐츠 최종 생성
-
-            MimeMessage message = mailSender.createMimeMessage();// 이메일 메시지 생성 객체
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);// T: html 형식, F: 텍스트 형식
-
-            helper.setFrom(senderEmail);
-            helper.setSubject(survey.getTitle());
-            helper.setTo(user.getEmail());
-            helper.setText(htmlContent, true);
-
-            // 이미지 첨부 (첨부파일로 cid를 사용)
-            ClassPathResource logoResource = new ClassPathResource("static/images/icon_logo.png");
-            helper.addInline("logoImage", logoResource); // 이미지 ID 'logoImage'로 첨부
-
-            ClassPathResource titleResource = new ClassPathResource("static/images/title.png");
-            helper.addInline("titleImage", titleResource); // 이미지 ID 'titleImage'로 첨부
-
-            log.info("link = {}", link);
-            mailSender.send(message);
-
+            mailSender(context, survey.getTitle(), user.getEmail(), link);
         } catch (MailException | MessagingException e){
             throw new RuntimeException("메일 발송 실패 ", e);
         } catch (Exception e) {
@@ -166,60 +144,25 @@ public class MailService {
         }
     }
 
+//    /**
+//     * 비밀번호 찾기 인증 메일
+//     * @param userId
+//     */
+//    public void sendPasswordModifyLink(String userId){
+//        log.info("userId = {}", userId);
+//        User user = userService.findByUserId(userId);
+//        String password = createCode();
+//        try{
+//            Context context = new Context();//타임리프 템플릿에 전달할 데이터 저장하는 컨테이너
+//            context.setVariable("inviterName", user.getName());
+//            context.setVariable("temporaryPassword", password);
+//
+//            mailSender(context, survey.getTitle(), user.getEmail(), link);
+//        } catch (MailException | MessagingException e){
+//            throw new RuntimeException("메일 발송 실패 ", e);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-    /**
-     * 비밀번호 찾기 인증 메일
-     * @param userId
-     */
-    public void sendPasswordModifyLink(String userId){
-        log.info("userId = {}", userId);
-        User user = userService.findByUserId(userId);
-        String password = createCode();
-        try{
-            Context context = new Context();//타임리프 템플릿에 전달할 데이터 저장하는 컨테이너
-            context.setVariable("inviterName", user.getName());
-            context.setVariable("temporaryPassword", password);
-
-            String htmlContent = templateEngine.process("mail/authentication", context);//타임리프 템플릿 처리 후 HTML 콘텐츠 최종 생성
-
-            MimeMessage message = mailSender.createMimeMessage();// 이메일 메시지 생성 객체
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);// T: html 형식, F: 텍스트 형식
-
-            helper.setFrom(senderEmail);
-            helper.setSubject("[Survwey] 인증번호 발급");
-            helper.setTo(user.getEmail());
-            helper.setText(htmlContent, true);
-
-            // 이미지 첨부 (첨부파일로 cid를 사용)
-            ClassPathResource logoResource = new ClassPathResource("static/images/icon_logo.png");
-            helper.addInline("logoImage", logoResource); // 이미지 ID 'logoImage'로 첨부
-
-            ClassPathResource titleResource = new ClassPathResource("static/images/title.png");
-            helper.addInline("titleImage", titleResource); // 이미지 ID 'titleImage'로 첨부
-
-            mailSender.send(message);
-
-        } catch (MailException | MessagingException e){
-            throw new RuntimeException("메일 발송 실패 ", e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //인증 번호
-    public String createCode() {
-        Random random = new Random();
-        StringBuffer key = new StringBuffer();
-
-        for (int i = 0; i < 8; i++) {
-            int index = random.nextInt(4);
-
-            switch (index) {
-                case 0: key.append((char) ((int) random.nextInt(26) + 97)); break;
-                case 1: key.append((char) ((int) random.nextInt(26) + 65)); break;
-                default: key.append(random.nextInt(9));
-            }
-        }
-        return key.toString();
-    }
 }
--- survwey/survwey/src/main/java/mcnc/survwey/global/redis/RedisConfig.java
+++ survwey/survwey/src/main/java/mcnc/survwey/global/redis/RedisConfig.java
@@ -2,6 +2,9 @@ package mcnc.survwey.global.redis;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mcnc.survwey.domain.mail.service.MailService;
+import mcnc.survwey.global.config.SessionContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,7 +16,16 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 @RequiredArgsConstructor
 public class RedisConfig {
 
+    private final MailService mailService;
 
+    @Value("${SURVEY_VERIFY_URL}")
+    private String notificationUrl;
+
+    /**
+     * 설문 결과 알림
+     * @param connectionFactory
+     * @return
+     */
     @Bean
     public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
@@ -25,7 +37,7 @@ public class RedisConfig {
                 Long surveyId = Long.parseLong(expiredKey.split(":")[2]);
                 //여기에 이메일 전송 로직
                 log.info("redis 이메일 전송: {}", surveyId);
-                //surveyEmailService.sendEmailToSurveyParticipants(surveyId);
+//                mailService.sendVerifySurveyLink(userId, surveyId, notificationUrl);
             }
         }, new PatternTopic("__keyevent@*__:expired"));
 
