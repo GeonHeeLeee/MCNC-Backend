86b2182adf14b777ca219f23c7b67d5e9fe6a0df
kangminlee1 <jack7853@naver.com>
2024-12-02 10:09:00 +0900

    refactor: Mail service, controller, Util 구조 및 메소드 이름 등 리팩토링

--- survwey/survwey/src/main/java/mcnc/survwey/domain/mail/service/MailService.java
+++ survwey/survwey/src/main/java/mcnc/survwey/domain/mail/service/MailService.java
@@ -48,7 +48,6 @@ public class MailService {
     private static final String TITLE_IMAGE_PATH = "static/images/title.png";
 
     //메일 보내는 메소드
-
     public void sendMail(Context context, String title, String email, String htmlPath) throws MessagingException {
         String htmlContent = templateEngine.process(htmlPath, context);//타임리프 템플릿 처리 후 HTML 콘텐츠 최종 생성
         MimeMessage message = mailSender.createMimeMessage();// 이메일 메시지 생성 객체
@@ -71,11 +70,12 @@ public class MailService {
 
     /**
      * 설문 초대
-     *
      * @param senderId
      * @param surveyId
+     * @param recipients
+     * @throws MessagingException
      */
-    public void sendLinkMessage(String senderId, Long surveyId, List<String> recipients) throws MessagingException {
+    public void sendInvitationLink(String senderId, Long surveyId, List<String> recipients) throws MessagingException {
         User sender = userService.findByUserId(senderId);
         Survey surveyToInvite = surveyService.findBySurveyId(surveyId);
         String encryptedLink = encryptLink(surveyToInvite.getSurveyId());
--- survwey/survwey/src/main/java/mcnc/survwey/domain/mail/utils/EncryptionUtil.java
+++ survwey/survwey/src/main/java/mcnc/survwey/domain/mail/utils/EncryptionUtil.java
@@ -20,6 +20,34 @@ public class EncryptionUtil {
     private String secretKey;
 
     /**
+     * 암/복호화 모드 설정
+     * @param urlParameter
+     * @param mode
+     * @return
+     */
+    public byte[] determinedCipherMode(String urlParameter, int mode){
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");//키 세팅 -> AES 기준으로 secretKey 부분에 들어오는 길이에 따라 암호화 방식 결정
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");//AES 알고리즘으로 CBC 모드, PKCS5Padding scheme 으로 초기화
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(secretKey.substring(0, 16).getBytes());//키를 16 바이트로 잘라 초기화 벡터 byte 로 변경
+            cipher.init(mode, keySpec, ivParameterSpec);//암호 모드 결정
+
+            if(mode == Cipher.ENCRYPT_MODE){//암호화(mode == 1)
+                return cipher.doFinal(urlParameter.getBytes());
+            }else if(mode == Cipher.DECRYPT_MODE){//복호화(mode == 2)
+                return cipher.doFinal(Base64.getDecoder().decode(urlParameter));
+            }else{
+                //mode == 3 -> 키 자체를 암호화(래핑)
+                //mode == 4 -> 래핑된 키를 복호화
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 암호 모드는 잘못된 방식입니다.");
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 링크는 잘못된 링크입니다.", e);
+        }
+
+    }
+
+    /**
      * AES/CBC 비밀키로 URL 파라미터 암호화
      * 16바이트 키 값은 환경변수로 저장됨
      * @param surveyId
@@ -27,13 +55,7 @@ public class EncryptionUtil {
      */
     public String encrypt(String surveyId) {
         try{
-            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-            // 암호화 스펙
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            IvParameterSpec ivParameterSpec = new IvParameterSpec(secretKey.substring(0, 16).getBytes());
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec); // 암호화 모드 초기화
-            byte[] encrypted = cipher.doFinal(surveyId.getBytes()); // 데이터 암호화
-            return Base64.getUrlEncoder().encodeToString(encrypted); // URL BASE64로 변환
+            return Base64.getUrlEncoder().encodeToString(determinedCipherMode(surveyId, Cipher.ENCRYPT_MODE));
         } catch (Exception e){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 링크는 잘못된 링크입니다.", e);
         }
@@ -48,21 +70,9 @@ public class EncryptionUtil {
      */
     public String decrypt(String encryptedUrl) {
         try{
-            // URL-safe Base64 문자열에서 `-`와 `_`를 각각 `+`와 `/`로 변환
+            // URL-safe Base64 문자열에서 `-`와 `_`를 각각 `+`와 `/`로 변환 -> URL 파라미터에 //로 나오는 경우 해결 (일반 Base64는  '-', '+' 처리)
             String base64DecodedUrl = encryptedUrl.replace("-", "+").replace("_", "/");
-
-            byte[] decodedBytes = Base64.getDecoder().decode(base64DecodedUrl);// Base64 디코딩
-
-            // 복호화 진행
-            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            IvParameterSpec ivParameterSpec = new IvParameterSpec(secretKey.substring(0, 16).getBytes());
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec); // 복호화 모드 초기화
-
-            byte[] decrypted = cipher.doFinal(decodedBytes); // 데이터 복호화
-            String result = new String(decrypted);
-
-            return result; // 복호화된 URL 반환
+            return new String(determinedCipherMode(base64DecodedUrl, Cipher.DECRYPT_MODE));
         }catch (Exception e){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 링크는 잘못된 링크입니다.", e);
         }
