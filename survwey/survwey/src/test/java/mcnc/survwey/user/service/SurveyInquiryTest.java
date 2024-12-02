cce60530d392528b74f8582655606f0ca8ba502b
GeonHee-Lee <ghlee00125@naver.com>
2024-12-02 12:41:45 +0900

    SurveyInquiry 테스트 일부 작성

--- survwey/survwey/src/test/java/mcnc/survwey/user/service/SurveyInquiryTest.java
+++ survwey/survwey/src/test/java/mcnc/survwey/user/service/SurveyInquiryTest.java
@@ -3,8 +3,13 @@ package mcnc.survwey.user.service;
 import mcnc.survwey.domain.enums.Gender;
 import mcnc.survwey.domain.enums.QuestionType;
 import mcnc.survwey.domain.question.Question;
+import mcnc.survwey.domain.question.repository.QuestionRepository;
 import mcnc.survwey.domain.selection.Selection;
+import mcnc.survwey.domain.selection.SelectionId;
+import mcnc.survwey.domain.selection.dto.SelectionResultDTO;
+import mcnc.survwey.domain.selection.repository.SelectionRepository;
 import mcnc.survwey.domain.survey.common.Survey;
+import mcnc.survwey.domain.survey.common.dto.SurveyWithDetailDTO;
 import mcnc.survwey.domain.survey.common.repository.SurveyRepository;
 import mcnc.survwey.domain.survey.inquiry.dto.SurveyWithCountDTO;
 import mcnc.survwey.domain.survey.inquiry.service.SurveyInquiryService;
@@ -32,8 +37,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @Transactional //테스트 후 롤백
 public class SurveyInquiryTest {
 
+    private static final int PAGE_NUMBER = 0;
+    private static final int PAGE_SIZE = 10;
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private SelectionRepository selectionRepository;
 
     @Autowired
     private SurveyRepository surveyRepository;
@@ -44,47 +58,18 @@ public class SurveyInquiryTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    private User testUser1;
+    private User testUser2;
     private Survey survey1;
     private Survey survey2;
 
-    private User testUser1;
-    private User testUser2;
-
     @BeforeEach
     void setUp() {
-        testUser1 = User.builder()
-                .userId("testUser1")
-                .name("testUser1")
-                .email("testUser1@test.com")
-                .password(passwordEncoder.encode("test1"))
-                .birth(LocalDate.from(LocalDateTime.now()))
-                .registerDate(LocalDateTime.now())
-                .gender(Gender.M)
-                .build();
+        testUser1 = createTestUser("testUser1", "testUser1@test.com", Gender.M);
+        testUser2 = createTestUser("testUser2", "testUser2@test.com", Gender.F);
 
-        testUser2 = User.builder()
-                .userId("testUser2")
-                .name("testUser2")
-                .password(passwordEncoder.encode("test2"))
-                .email("testUser2@test.com")
-                .birth(LocalDate.from(LocalDateTime.now()))
-                .registerDate(LocalDateTime.now())
-                .gender(Gender.F)
-                .build();
-
-        survey1 = Survey.builder()
-                .expireDate(LocalDateTime.now().plusDays(30))
-                .title("survey1")
-                .description("survey1 description")
-                .user(testUser1)
-                .build();
-
-        survey2 = Survey.builder()
-                .expireDate(LocalDateTime.now().plusDays(30))
-                .title("survey2")
-                .description("survey2 description")
-                .user(testUser1)
-                .build();
+        survey1 = createTestSurvey(testUser1, "survey1");
+        survey2 = createTestSurvey(testUser1, "survey2");
 
         userRepository.save(testUser1);
         userRepository.save(testUser2);
@@ -94,12 +79,12 @@ public class SurveyInquiryTest {
 
     @Test
     @DisplayName("사용자가 만든 설문 조회 성공 테스트")
-    public void 사용자가_만든_설문_조회_성공_테스트() {
-        //when
+    public void testSuccessfulGetUserCreatedSurveyList() {
+        // When
         Page<SurveyWithCountDTO> result = surveyInquiryService.getUserCreatedSurveyList(
-                testUser1.getUserId(), 0, 10);
+                testUser1.getUserId(), PAGE_NUMBER, PAGE_SIZE);
 
-        //then
+        // Then
         assertNotNull(result);
         assertEquals(2, result.getTotalElements());
         assertEquals("survey1", result.getContent().get(0).getTitle());
@@ -107,34 +92,85 @@ public class SurveyInquiryTest {
 
     @Test
     @DisplayName("사용자가 만든 설문 조회 실패 테스트")
-    public void 사용자가_만든_설문_조회_실패_테스트() {
-        //when
+    public void testFailedGetUserCreatedSurveyList() {
+        // When
         Page<SurveyWithCountDTO> result = surveyInquiryService.getUserCreatedSurveyList(
-                testUser2.getUserId(), 0, 10);
+                testUser2.getUserId(), PAGE_NUMBER, PAGE_SIZE);
 
-        //then
+        // Then
         assertNotNull(result);
         assertEquals(0, result.getTotalElements());
     }
 
     @Test
     @DisplayName("특정 설문 조회 성공 테스트")
-    public void 특정_설문_조회_성공_테스트() {
-        //given
-        Question question = Question.builder()
-                .body("질문1")
-                .survey(survey1)
-                .type(QuestionType.SUBJECTIVE)
-                .build();
+    public void testSuccessfulGetSurveyWithDetail() {
+        // Given
+        Question question1 = createTestQuestion(survey1, "질문1", QuestionType.SUBJECTIVE);
+        Question question2 = createTestQuestion(survey1, "질문2", QuestionType.OBJ_SINGLE);
 
-        for()
+        questionRepository.save(question1);
+        questionRepository.save(question2);
+
+        Selection selection1 = createTestSelection(question2, "선택1", false);
+        Selection selection2 = createTestSelection(question2, "선택2", true);
+
+        selectionRepository.save(selection1);
+        selectionRepository.save(selection2);
+        surveyRepository.save(survey1);
+
+        // When
+        SurveyWithDetailDTO surveyWithDetail = surveyInquiryService.getSurveyWithDetail(survey1.getSurveyId());
+
+        // Then
+        assertEquals(surveyWithDetail.getSurveyId(), survey1.getSurveyId());
+        assertEquals(surveyWithDetail.getQuestionList().size(), 2);
+        assertEquals(surveyWithDetail.getQuestionList().get(1).getSelectionList().size(), 2);
+    }
+
+//    @Test
+//    @DisplayName("특정 설문 조회 실패 테스트 - 설문 아이디 존재하지 않음")
+//    //Giv
+
+    private User createTestUser(String userId, String email, Gender gender) {
+        return User.builder()
+                .userId(userId)
+                .name(userId)
+                .email(email)
+                .password(passwordEncoder.encode(userId + "Pass"))
+                .birth(LocalDate.now())
+                .registerDate(LocalDateTime.now())
+                .gender(gender)
+                .build();
+    }
+
+    private Survey createTestSurvey(User user, String title) {
+        return Survey.builder()
+                .expireDate(LocalDateTime.now().plusDays(30))
+                .title(title)
+                .description(title + " description")
+                .user(user)
+                .build();
+    }
+
+    private Question createTestQuestion(Survey survey, String body, QuestionType type) {
+        Question question = Question.builder()
+                .body(body)
+                .survey(survey)
+                .type(type)
+                .build();
+        survey.addQuestion(question);
+        return question;
+    }
+
+    private Selection createTestSelection(Question question, String body, boolean isEtc) {
         Selection selection = Selection.builder()
-                .body("선택1")
-                .isEtc(false)
+                .id(new SelectionId(question.getQuesId(), question.getSelectionList().size()))
+                .body(body)
+                .isEtc(isEtc)
                 .question(question)
                 .build();
-        //when
-
-        //then
+        question.addSelection(selection);
+        return selection;
     }
 }
