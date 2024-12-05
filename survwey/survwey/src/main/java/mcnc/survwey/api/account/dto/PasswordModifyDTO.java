04b83ee922d18d0b777a7ed510c0fa1990f87805
GeonHee-Lee <ghlee00125@naver.com>
2024-12-05 16:50:15 +0900

    fix: 암호화/복호화 로직 오류 수정

--- survwey/survwey/src/main/java/mcnc/survwey/api/account/dto/PasswordModifyDTO.java
+++ survwey/survwey/src/main/java/mcnc/survwey/api/account/dto/PasswordModifyDTO.java
@@ -16,8 +16,8 @@ public class PasswordModifyDTO {
     @Pattern(regexp = "^[a-zA-Z0-9-]+$", message = "사용자 아이디는 영문과 숫자의 조합이어야 합니다.")
     private String userId;
 
-    @NotBlank(message = "비밀번호는 필수입니다.")
     @DecryptField
+    @NotBlank(message = "비밀번호는 필수입니다.")
     @Pattern(
             regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+]).{8,}$",
             message = "비밀번호는 최소 8자, 숫자, 특수문자 및 대소문자를 포함해야 합니다."
