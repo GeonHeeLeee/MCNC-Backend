aba7fd62137492f1877cb69ba83b98adcb4e239f
GeonHee-Lee <ghlee00125@naver.com>
2024-12-13 09:14:31 +0900

    refactor: QueryDSL 주석 작성

--- survwey/survwey/src/main/java/mcnc/survwey/domain/objAnswer/repository/queryDSL/ObjAnswerRepositoryCustom.java
+++ survwey/survwey/src/main/java/mcnc/survwey/domain/objAnswer/repository/queryDSL/ObjAnswerRepositoryCustom.java
@@ -5,5 +5,6 @@ import mcnc.survwey.domain.objAnswer.ObjAnswer;
 import java.util.List;
 
 public interface ObjAnswerRepositoryCustom {
+    //사용자가 응답한 객관식 응답 조회
     List<ObjAnswer> findUserRespondedAnswer(Long surveyId, String userId);
 }
